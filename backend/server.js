e36c671653a1610ea9d30d7b23f2d3a74c6b7133
Cap_pool <abhinavg90834@gmail.com>
2021-08-10 16:22:18 +0530

    sec 3

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const products = require("./products");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hii");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is running in ${process.env.NODE_ENV} on ${port}`);
+});
